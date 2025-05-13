3b9434b63484917e681072cbc61dd153f8131c5a
fatimah oladipo <fatimaholadipo4@gmail.com>
2025-05-13 21:59:03 +0100

    debugged error in resource view

--- src/app/Home/resourceView/page.tsx
+++ src/app/Home/resourceView/page.tsx
@@ -5,7 +5,7 @@ export default function Page(){
     return(
         <Suspense fallback={<div className="flex h-[100%] flex-col items-center justify-center">
   <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-blue-500"></div>
-  <p className="mt-2 text-gray-500">Loading your study plan...</p>
+  <p className="mt-2 text-gray-500">Loading video...</p>
 </div>}>
             <ResourceViewer />
         </Suspense>
