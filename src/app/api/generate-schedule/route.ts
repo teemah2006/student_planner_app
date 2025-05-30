4b7d942b6ae8d7d710f1a514aefbc54ed1516f38
fatimah oladipo <fatimaholadipo4@gmail.com>
2025-05-30 03:23:48 +0100

    change in generate reco

--- src/app/api/generate-schedule/route.ts
+++ src/app/api/generate-schedule/route.ts
@@ -109,3 +109,41 @@ Instructions:
     return NextResponse.json({ error: `Failed to generate schedule ${error}` }, { status: 500 });
   }
 }
+
+// import { NextRequest, NextResponse } from "next/server";
+// import { adminAuth, adminDb } from "../../../../utils/firebaseAdmin";
+// export async function DELETE(req: NextRequest) {
+//   const authHeader = req.headers.get("authorization");
+//   const token = authHeader?.split("Bearer ")[1];
+
+//   if (!token) {
+//     return NextResponse.json({ error: "No token provided" }, { status: 401 });
+//   }
+
+//   const body = await req.json();
+//   const { recommendationId } = body;
+
+//   if (!recommendationId) {
+//     return NextResponse.json({ error: "No recommendation ID provided" }, { status: 400 });
+//   }
+
+//   try {
+//     const decodedToken = await adminAuth.verifyIdToken(token);
+//     const uid = decodedToken.uid;
+
+//     const recRef = adminDb
+//       .collection("users")
+//       .doc(uid)
+//       .collection("recommendations")
+//       .doc(recommendationId);
+    
+  
+
+//     // await recRef.delete();
+
+//     return NextResponse.json({ success: true, message: "Deleted successfully" }, { status: 200 });
+//   } catch (err) {
+//     console.error("Delete error:", err);
+//     return NextResponse.json({ error: "Failed to delete" }, { status: 500 });
+//   }
+// }
